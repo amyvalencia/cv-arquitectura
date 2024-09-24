ee3738c66a8af94e678ed96406520001cb473568
amyvalencia <michel10diez@gmail.com>
2024-09-24 11:30:08 -0600

    Primera versión

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("menuToggle").addEventListener("click", function() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "flex") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+        menu.style.flexDirection = "column";
+    }
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
